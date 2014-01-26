f4ea496409f044f9c0bad3d84d56fa6eea1f7575
Jose Antonio Quevedo <joseantonio.quevedo@gmail.com>
2014-01-26 18:10:29 +0100

    Added chrootBuilder tool.

---
+++ chroot-builder/bin/config_chroot_second_stage.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+#############################################################
+# Author: Jose Antonio Quevedo <joseantonio.quevedo@gmail.com>
+# Date: 2013-2014
+#
+# Based on: http://linux-sunxi.org/Debian
+#
+# NOTE: This script will be run whithin the chroot AS ROOT!
+#############################################################
+
+#set -x
+
+INIT_PWD=$(pwd)
+
+cd $(dirname $0)
+BASEDIR=$(pwd)
+
+##################################
+#VARIABLES
+
+#CHROOT_NAME=$
+myHostName=myChroot
+
+## FUNCIONES
+
+function configSourcesList(){
+
+#cat <<EOF
+#END > /etc/apt/sources.list
+echo "deb http://ftp.us.debian.org/debian/ wheezy main contrib non-free
+deb-src http://ftp.us.debian.org/debian/ wheezy main contrib non-free
+deb http://ftp.us.debian.org/debian/ wheezy-updates main contrib non-free
+deb-src http://ftp.us.debian.org/debian/ wheezy-updates main contrib non-free
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free"\
+>> /etc/apt/sources.list
+#END
+#EOF
+
+apt-get update
+}
+
+function configLanguage(){
+
+  ## Language
+    export LANG=C
+
+    dpkg-reconfigure locales
+# Choose en_US.UTF-8 for both prompts, or whatever you want.
+    export LANG=en_US.UTF-8
+
+
+}
+
+function configKeyboard(){
+
+    dpkg-reconfigure keyboard-configuration -phigh
+
+}
+
+function installDeps(){
+
+    MIN_DEPS="netbase"
+    SYSTEM_DEPS="udev"
+    NETWORKING_DEPS="ifupdown net-tools iproute iputils-ping ntpdate ntp dhcp3-client"
+    APT_DEPS="apt-utils dialog"
+    KEYBOARD_DEPS="keyboard-configuration"
+    NET_TOOLS="wget"
+    SYSTEM_TOOLS="mc"
+    OPTIONAL_DEPS="dropbear"
+    NOT_NEEDED_DEPS="dialog locales vim nano less tzdata console-tools module-init-tools"
+    ZIGBEE_DEPS="python-serial"
+
+    apt-get update
+
+    apt-get install $MIN_DEPS $SYSTEM_DEPS $NETWORKING_DEPS $APT_DEPS $KEYBOARD_DEPS $NET_TOOLS $SYSTEM_TOOLS $OPTIONAL_DEPS #$NOT_NEEDED_DEPS
+
+}
+
+function configNetwork(){
+
+# Configure Network
+#    cat <<EOF
+#END > /etc/network/interfaces
+echo "auto lo eth0
+allow-hotplug eth0
+iface lo inet loopback
+iface eth0 inet dhcp" \
+> /etc/network/interfaces
+#END
+
+#EOF
+
+
+}
+
+function setHostname(){
+    echo $myHostName > /etc/hostname
+}
+
+function configFilesystem(){
+
+    cat <<EOF
+END > /etc/fstab
+# /etc/fstab: static file system information.
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+/dev/root      /               ext4    noatime,errors=remount-ro 0 1
+tmpfs          /tmp            tmpfs   defaults          0       0
+END
+EOF
+
+}
+
+function activateRemoteconsole(){
+
+## Activate remote console
+    echo 'T0:2345:respawn:/sbin/getty -L ttyS0 115200 linux' >> /etc/inittab
+    sed -i 's/^\([3-6]:.* tty[3-6]\)/#\1/' /etc/inittab
+
+}
+
+function configureChroot(){
+
+    configSourcesList
+    installDeps
+
+#    configLanguage
+    configKeyboard
+
+    configNetwork
+    setHostname
+
+    ## Config Filesystem
+    configFilesystem
+    activateRemoteconsole
+
+
+    ## Set root passwd
+    passwd
+    exit
+
+}
+
+
+#main(){
+
+configureChroot
+
+#}
+
---
+++ chroot-builder/chrootBuilder.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Author: Jose Antonio Quevedo <joseantonio.quevedo@gmail.com>
+# Date: 2013-2014
+
+# Based on: http://linux-sunxi.org/Debian
+
+#set -x 
+set -e
+
+INIT_PWD=$(pwd)
+
+cd $(dirname $0)
+BASEDIR=$(pwd)
+
+##################################
+#VARIABLES
+CHROOT_NAME=binary
+TEST_DIR=chroot
+IMAGE_FILE_EXT=ext4
+
+SECOND_STAGE_SCRIPT_FILENAME=config_chroot_second_stage.sh
+SECOND_STAGE_SCRIPT=$BASEDIR/bin/$SECOND_STAGE_SCRIPT_FILENAME
+DEST_SECOND_STAGE_FILENAME_CHROOT=/usr/bin/$SECOND_STAGE_SCRIPT_FILENAME
+DEST_SECOND_STAGE_FILE=$BASEDIR/$TEST_DIR/$CHROOT_NAME/$DEST_SECOND_STAGE_FILENAME_CHROOT
+
+
+## FUNCTIONS
+
+function parada(){
+    echo "Parada de segundos.."
+    sleep 10
+
+}
+
+function buildDebootstrap(){
+
+    mkdir $TEST_DIR || true
+
+    cd $TEST_DIR
+
+    mkdir $CHROOT_NAME || true
+
+    OUTPUT_IMAGE=$CHROOT_NAME.$IMAGE_FILE_EXT
+    [ -e $OUTPUT_IMAGE ] || (
+    	dd if=/dev/zero of=$OUTPUT_IMAGE bs=1M count=2048
+	/sbin/mkfs.ext4 -F $OUTPUT_IMAGE
+    )
+    sudo mount -o loop $OUTPUT_IMAGE $CHROOT_NAME || true
+
+
+#    [ -d $CHROOT_NAME/debootsrap -o -d $CHROOT_NAME/usr/ ] ||
+    [ -e $CHROOT_NAME/debootstrap/debootstrap ] || (
+	sudo debootstrap --verbose --download-only --arch armhf --variant=minbase --foreign wheezy $CHROOT_NAME http://ftp.us.debian.org/debian
+	sudo debootstrap --verbose --arch armhf --variant=minbase --foreign wheezy $CHROOT_NAME http://ftp.es.debian.org/debian || true
+    )
+#    parada
+
+    sudo mkdir -p $CHROOT_NAME/dev/pts | true
+    sudo mkdir -p $CHROOT_NAME/proc || true
+    sudo modprobe binfmt_misc
+    sudo mount -t devpts devpts $CHROOT_NAME/dev/pts || true
+    sudo mount -t proc proc $CHROOT_NAME/proc || true
+
+
+}
+
+function copySecondStage2chroot(){
+    sudo cp $SECOND_STAGE_SCRIPT $DEST_SECOND_STAGE_FILE
+    sudo chmod 755 $DEST_SECOND_STAGE_FILE
+    sudo chown root:root $DEST_SECOND_STAGE_FILE
+}
+
+
+# main(){
+
+#sudo aptitude install debootstrap qemu-user-static binfmt-support
+
+buildDebootstrap
+
+echo " At the end, you should see \"I: Base system installed successfully.\""
+
+parada
+copySecondStage2chroot
+# Compatibilidad arm
+sudo cp /usr/bin/qemu-arm-static $CHROOT_NAME/usr/bin/
+
+#parada
+echo "DEBOOTSTRAP SECOND-STAGE:"
+sudo chroot $CHROOT_NAME /debootstrap/debootstrap --second-stage
+
+#parada
+echo "CUSTOMIZING CHROOT:"
+sudo chroot $CHROOT_NAME $DEST_SECOND_STAGE_FILENAME_CHROOT
+
+echo "Comprimiendo chroot en $CHROOT_NAME.tar.gz"
+sudo tar -czf $CHROOT_NAME.tar.gz $CHROOT_NAME
+
+
+#exit 
+
+# echo
+# echo "Take a look at: http://linux-sunxi.org/FirstSteps"
+# echo "to build u-boot, kernel, script.bin and boot.cmd."
+# echo
+
+#}
+
+
